b6d68f49c297e38123df083145bfed0331c52d2e
NicoTortorici <91245671+NicoTortorici@users.noreply.github.com>
2022-01-19 18:43:04 +0100

    Added login and message receivers.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const Logger = require('nodemon/lib/utils/log');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
+
+var sockets = {};
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
@@ -11,6 +14,21 @@ const server = express()
   const io = socketIO(server);
 
   io.on('connection', (socket) => {
+    var username = null;
+
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
+
+    socket.on('login', name => {
+      sockets[name] = socket;
+      username = name;
+
+      console.log('User logged in: ' + name);
+    });
+
+    socket.on('message', args => {
+      if (receiver in sockets) {
+        sockets[receiver].emit('message', args['username'], args['content']);
+      }
+    })
   });
