272ab4f62551ca6977a84091a312b22ac57aa03c
NicoTortorici <91245671+NicoTortorici@users.noreply.github.com>
2022-01-19 17:33:40 +0100

    Back to socket.io

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-import WebSocket from 'ws';
+const { server } = require('websocket');
 
-const ws = new WebSocket('ws://localhost');
+const app = require('express')()
 
-ws.on('open', function open() {
-  console.log('connection open');
-  ws.send('something');
-});
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
+app.get('/', (req, res) => {
+  console.log('server is running') 
+  //res.send("Node Server is running. Yay!!")
+})
 
-ws.on('message', function message(data) {
-  console.log('received: %s', data);
-});
+io.on('connection',socket => {
+    console.log('a user connected'); 
+    });
+
+server.listen();
