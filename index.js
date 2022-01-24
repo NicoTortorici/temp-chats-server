b1589662c19c007a2b85ec57d10e088a2e0ddb65
NicoTortorici <91245671+NicoTortorici@users.noreply.github.com>
2022-01-24 13:30:07 +0100

    Fixed identation.

--- index.js
+++ index.js
@@ -10,37 +10,37 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-  const io = socketIO(server);
+const io = socketIO(server);
 
-  io.on('connection', (socket) => {
-    var username = null;
+io.on('connection', (socket) => {
+  var username = null;
 
-    console.log('Client connected');
-    socket.on('disconnect', () => {
-      console.log(username + ' disconnected')
-      if (username in sockets) {
-        console.log('Loggin ' + username + ' out.');
-        delete sockets[username];
-      }
-    });
-
-    socket.on('login', (name) => {
-      if (name in sockets) {
-        socket.emit('login', 'Name already in use.');
-        return;
-      }
-
-      sockets[name] = socket;
-      username = name;
-
-      console.log('User logged in: ' + name);
-
-      socket.emit('login', 'ok');
-    });
-
-    socket.on('message', args => {
-      if (args['receiver'] in sockets) {
-        sockets[args['receiver']].emit('message', { 'sender': username, 'content': args['content'] });
-      }
-    })
+  console.log('Client connected');
+  socket.on('disconnect', () => {
+    console.log(username + ' disconnected')
+    if (username in sockets) {
+      console.log('Loggin ' + username + ' out.');
+      delete sockets[username];
+    }
   });
+
+  socket.on('login', (name) => {
+    if (name in sockets) {
+      socket.emit('login', 'Name already in use.');
+      return;
+    }
+
+    sockets[name] = socket;
+    username = name;
+
+    console.log('User logged in: ' + name);
+
+    socket.emit('login', 'ok');
+  });
+
+  socket.on('message', args => {
+    if (args['receiver'] in sockets) {
+      sockets[args['receiver']].emit('message', { 'sender': username, 'content': args['content'] });
+    }
+  });
+});
